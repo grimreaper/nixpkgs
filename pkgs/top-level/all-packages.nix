f4f371acfa848ac0f81e99200e2bb2d29c3b1177
Andreas Rammhold <andreas@rammhold.de>
2017-12-31 15:47:05 +0100

    libdazzle: init at 3.28.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9596,6 +9596,8 @@ with pkgs;
 
   libdap = callPackage ../development/libraries/libdap { };
 
+  libdazzle = callPackage ../development/libraries/libdazzle { };
+
   libdbi = callPackage ../development/libraries/libdbi { };
 
   libdbiDriversBase = callPackage ../development/libraries/libdbi-drivers {
